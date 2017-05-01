73d0676c55fd534d28815777f4852a6a1acdb9e6
armsarun <arunkumarp93@yahoo.com>
2017-05-01 14:59:38 +0530

    dequeue example updated

--- queue/deque.py
+++ queue/deque.py
@@ -1,18 +1,22 @@
 from collections import deque
 """
 python ADT has deque in collection
-To import the element rightside
+To insert the element:
+rightside
 append(value)
 leftside
 appendleft(value)
+
+To remove element:
 popleft
 popleft()
 popright
 pop()
+
+
 """
 def palindrome(string):
     findpal = deque(string)
-    print deque(reversed(findpal))
     stillEqual = True
     while len(findpal) > 1 and stillEqual:
         front = findpal.pop()
