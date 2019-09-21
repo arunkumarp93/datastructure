09d41737c22243e48cbbcfba9d9857a80cc0be5a
Arun Kumar <armsarun@users.noreply.github.com>
2019-09-21 09:47:38 +0530

    Update mergesort.py

--- sorting/mergesort.py
+++ sorting/mergesort.py
@@ -1,73 +1,99 @@
-def mergesort_helper(alist):
 
-    list_length = len(alist)
+def merge(left, right):
+    """
+    Arguments:
+    --------------------
+    left[list]: left array
+    right[list]: right array
     
-    if list_length > 1:
-        """
-        split the list until it becomes
-        to 1 uing recursive mergeSort(left),mergeSort(right)
-        """
-        
-        """
-            To run algorithm in best case O(n log n) we need to pass the
-            start and end of the list instead of slice
-            def mergeSort(Arr, start, end) {
-
-	            if(start < end) :
-		            mid = (start + end) / 2
-		            mergeSort(Arr, start, mid)
-		            mergeSort(Arr, mid+1, end)
-		            merge(Arr, start, mid, end)
-        """
-
-        mid = list_length // 2
-        start = 0
-        end = list_length
-        lefthalf = alist[start:mid]
-        righthalf = alist[mid:end]
-                
-        mergesort_helper(lefthalf)
-        mergesort_helper(righthalf)
+    return merge[list]
+    --------------------
     
-        leftcount=0
-        rightcount=0
-        mergecount=0
-        
-        """
-            compare the left and right values and add minimum
-            value to the un_list[mcount] then increase the mcount position
-            check the left seprate and right seprate and add to
-            un_list[mcount]
-        """
-        leftlength = len(lefthalf)
-        rightlength = len(righthalf)
-        while leftcount < leftlength and rightcount < rightlength:
-            if lefthalf[leftcount] < righthalf[rightcount]:
-                alist[mergecount] = lefthalf[leftcount]
-                leftcount += 1
-            else:
-                alist[mergecount] = righthalf[rightcount]
-                rightcount += 1
-            mergecount += 1
-                
-	"""Some cases we left with either left or right side with elements . we need to add those to merge array
- 	  e.g., left = [5] and right =[5]
-	  above loop add the left to merge array and failed now right array value we need to add to merge array so below 
-	  loops does that work
-	"""
-        while leftcount < leftlength:
-            alist[mergecount] = lefthalf[leftcount]
-            leftcount += 1
-            mergecount += 1
-        
-        while rightcount < rightlength:
-            alist[mergecount] = righthalf[rightcount]
-            rightcount += 1
-            mergecount += 1
-    return alist
+    Check the right and left of the element and continuously and
+    add values to temp array
     
-def mergesort(unsorted):
-    print (mergesort_helper(unsorted))
+    if any values remaining in left or right of the array
+    will added separately by checking left and right
+    
+    [10] [5]
+    
+    first loop checks the and add 10 to temp. Now 5 is left
+    it'll be added by right loop
+    
+    """
+    temp = []
+    leftindex = 0
+    rightindex = 0
+    
+    leftlength = len(left)
+    rightlength = len(right)
+    
+    while leftindex < leftlength and rightindex < rightlength:
+        leftvalue = left[leftindex]
+        rightvalue = right[rightindex]
+        if leftvalue < rightvalue:
+            temp.append(leftvalue)
+            leftindex += 1
+        else:
+            temp.append(rightvalue)
+            rightindex += 1
+    
+    while leftindex < leftlength:
+        temp.append(left[leftindex])
+        leftindex += 1
+    
+    while rightindex < rightlength:
+        temp.append(right[rightindex])
+        rightindex += 1
+        
+    return temp  
+    
+def divider(array, start, end):
+    """
+    Arguments:
+    ----------------------
+    array[list]: list of elements to merge
+    start[int]: starting value of list
+    end[int]: ending value of list
+    ----------------------
+    
+    Split the array until one element
+    [1,2,3,4]-->
+    [1,2] [3,4]
+    [1] [2] [3] [4]
+    
+    Then pass the left and right of the array to merge
+    """
+    if len(array)  > 1:
+        mid = len(array)//2
+        left  = array[0:mid]
+        right = array[mid:end]
+        sorted_left = divider(left, start, mid)
+        sorted_right = divider(right, mid+1, end)
+        return merge(sorted_left, sorted_right)
+    else:
+	# return array so it'll be added to sorted variable and pass to merge 
+	# and get sorted list 
+        return array
+        
+    """
+        Alternative code of above
+        if len(array) <= 1:
+            return array
+        
+        mid = len(array)//2
+        left  = array[0:mid]
+        right = array[mid:end]
+        sorted_left = divider(left, start, mid)
+        sorted_right = divider(right, mid+1, end)
+        return merge(sorted_left, sorted_right)
+        
+    """
 
 
-mergesort([12,55,34,66,45,99,100,1,23])
+
+def mergesort(array):
+    divider(array, 0, len(array))
+    
+mergesort([10,5,4,11,3,8,2,1])
+    
